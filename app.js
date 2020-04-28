7b368d7a35b43bb1ab194582b560c91fe26e178e
Cesar Morales <cesarlenin2@live.com>
2020-04-28 08:19:15 -0700

    blue print

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+// const cors = require('cors');
+
+const app = express();
+
+// app.use(cors());
+
+app.use(morgan('common'));
+
+app.get('/apps', (req, res) => {
+
+});
+
+app.listen(8000, () => {
+  console.log('Server started on PORT 8000');
+});
