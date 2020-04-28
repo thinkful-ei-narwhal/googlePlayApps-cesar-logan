1014155da2f7bf11b8fc1e01bdf2d3536f499487
Cesar Morales <cesarlenin2@live.com>
2020-04-28 08:36:29 -0700

    sort filter started

--- app.js
+++ app.js
@@ -10,11 +10,34 @@ const app = express();
 
 app.use(morgan('common'));
 
-const apps= require('playstore.js');
+const apps= require('./playstore.js');
 
 app.get('/apps', (req, res) => {
-    
+  const {search= '', sort } = req.query;
 
+  if (sort) {
+    if (!['app', 'rating'].includes(sort)) {
+      return res
+        .status(400)
+        .send('Sort must be one of app or rating');
+    }
+  }
+
+  let results = apps
+    .filter(app =>
+      app
+        .App
+        .toLowerCase()
+        .includes(search.toLowerCase()));
+
+  if (sort) {
+    results
+      .sort((a, b) => {
+        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+      });
+  }
+
+  res.json(results);
 });
 
 app.listen(8000, () => {
